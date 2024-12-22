0fb1a784212b460230cdf21f3be06d1420062ce5
AfolabiOmobolanle <afolabiomobolanle00@gmail.com>
2024-12-22 11:57:29 +0100

    reverted back to exoress session

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const session = require('express-session');
-const connectRedis = require('connect-redis');
-const redis = require('redis');
 const path = require('path');
-const fs = require('fs');
 const dotenv = require('dotenv');
 
-dotenv.config(); // Load environment variables from .env
-
-// Redis Client
-const redisClient = redis.createClient({
-  url: process.env.REDIS_URL, // or use your redis connection URL directly
-});
+dotenv.config(); 
 
 // Express app setup
 const app = express();
-const RedisStore = connectRedis(session); // connectRedis is now directly used with session
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Express session setup
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }), // Use RedisStore with session
-    secret: process.env.SESSION_SECRET || 'your-session-secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 1000 * 60 * 30 },
+    secret: process.env.SESSION_SECRET,
+    resave: false, // Do not save session if unmodified
+    saveUninitialized: false, // Do not create session until something is stored
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // Secure cookies for production
+      maxAge: 1000 * 60 * 30, // Session expiration time (30 minutes)
+    },
   })
 );
 
@@ -34,7 +28,7 @@ app.use(
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Your API routes
-app.use('/api/v1/user', require('./routes/users'));
+app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/visitor', require('./routes/visitors'));
 app.use('/api/v1/products', require('./routes/products'));
 
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,6 @@ const { saveUser,getTotalUsers } = require('../controller/users.controller.js');
 
 // Route to save user data
 router.post('/create', saveUser);
-router.get('/total', (req, res) => {
-  getTotalUsers((err, count) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.status(200).json({ userCount: count });
-  });
-});
-
+router.get('/total', getTotalUsers)
 
 module.exports = router;
